c4812cbfb342430b7ae60d23e49216559c4f1bd7
zhenghao <hao.zheng89@hotmail.com>
2021-06-04 22:14:47 -0500

    update

--- csvProcessor.java
+++ csvProcessor.java
@@ -8,223 +8,246 @@ import java.util.*;
  * This code is used for processing COVID-19 metadata .csv file, including:
  * 1) Split the original files into multiple smaller sized files by year values then output them.
  * 2) Generate multiple files by hospitalization status and then regions for each week, month, and quarter.
- * 3) todo: correct the monthly file generation section to make it more precisely.
- * 4) todo: calculate input data for statistic purposes.
+ * 3) todo: calculate input data for statistic purposes.
  */
 public class csvProcessor
 {
 	private static ArrayList<String[]> rows;	// store all data in an entire .csv file
 	private static final int WEEKS = 52;	// max value of COV_EW is 52 (except unknown)
-	private static String nameYear;	// label for directory name
-	private static int startWeek;	// label for counting week
+	private static final int HPSTATUS = 3;
 
 	/* Main method */
 	public static void main(String[] args)
 	{
 		/* Initialize variables */
-		String filePath = "";	// may require changes
-		String fileName = "COVID19-eng.csv";
+		String srcPath = "";	// may require changes
+		String srcName = "COVID19-eng.csv";
 		rows = new ArrayList<>();
-		int colRegion = 1;
 		int colWeek = 2;
 		int colYear = 4;
 		int colIcu = 11;
+		int colDeath = 15;
 
 		/* Read file */
-		readCSV(filePath + fileName);
+		readCSV(srcPath + srcName);
 
 		/* Split the original file by years, output new files */
-		splitCSV(filePath, rows, colYear, 20);
-		splitCSV(filePath, rows, colYear, 21);
-		splitCSV(filePath, rows, colYear, 99);
+		splitCSV_year(rows, colYear, 20);
+		splitCSV_year(rows, colYear, 21);
+		splitCSV_year(rows, colYear, 99);
 
-		/* 1st screening: select entries that have required COV_HSP values */
-		ArrayList<String[]> hp_icu = getEntry(rows, colIcu, 1);
-		ArrayList<String[]> hp_non_icu = getEntry(rows, colIcu, 2);
-		ArrayList<String[]> non_hp = getEntry(rows, colIcu, 3);
+		/* select entries that have required COV_HSP values each year in Canada */
+		ArrayList<String[]> year20 = getEntry(rows, colYear, 20);
+		ArrayList<String[]> year21 = getEntry(rows, colYear, 21);
 
-		/* 2nd screening: based on the 1st screening, split entries by different COV_REG values */
-		// when COV_HSP = 1
-		ArrayList<String[]> AC1 = getEntry(hp_icu, colRegion, 1);	//Atlantic
-		ArrayList<String[]> QC1 = getEntry(hp_icu, colRegion, 2);	//Quebec
-		ArrayList<String[]> ON1 = getEntry(hp_icu, colRegion, 3);	//Ontario and Nunavut
-		ArrayList<String[]> PS1 = getEntry(hp_icu, colRegion, 4);	//Prairies
-		ArrayList<String[]> BY1 = getEntry(hp_icu, colRegion, 5);	//British Columbia and Yukon
+		// hp_status_xx.get(0): entries in Canada that are hospitalized and in ICU
+		// hp_status_xx.get(1): entries in Canada that are hospitalized but not in ICU
+		// hp_status_xx.get(2): entries in Canada that are not hospitalized
+		// xx: year (i.e. 20 means year 2020)
+		ArrayList<ArrayList<String[]>> hp_status_20 = new ArrayList<>();
+		for (int i = 1; i <= HPSTATUS; i++) {
+			ArrayList<String[]> temp = getEntry(year20, colIcu, i);
+			hp_status_20.add(temp);
+		}
 
-		// when COV_HSP = 2
-		ArrayList<String[]> AC2 = getEntry(hp_non_icu, colRegion, 1);
-		ArrayList<String[]> QC2 = getEntry(hp_non_icu, colRegion, 2);
-		ArrayList<String[]> ON2 = getEntry(hp_non_icu, colRegion, 3);
-		ArrayList<String[]> PS2 = getEntry(hp_non_icu, colRegion, 4);
-		ArrayList<String[]> BY2 = getEntry(hp_non_icu, colRegion, 5);
+		ArrayList<ArrayList<String[]>> hp_status_21 = new ArrayList<>();
+		for (int i = 1; i <= HPSTATUS; i++) {
+			ArrayList<String[]> temp = getEntry(year21, colIcu, i);
+			hp_status_21.add(temp);
+		}
 
-		// when COV_HSP = 3
-		ArrayList<String[]> AC3 = getEntry(non_hp, colRegion, 1);
-		ArrayList<String[]> QC3 = getEntry(non_hp, colRegion, 2);
-		ArrayList<String[]> ON3 = getEntry(non_hp, colRegion, 3);
-		ArrayList<String[]> PS3 = getEntry(non_hp, colRegion, 4);
-		ArrayList<String[]> BY3 = getEntry(non_hp, colRegion, 5);
-
-		/* Generate new files, as per week, month, and quarter */
-		String[] dirHpstat = {"hp_status/hp_icu/", "hp_status/hp_non_icu/", "hp_status/non_hp/"};
-		String[] dirRegion = {"Atlantic/", "Quebec/", "Ontario & Nunavut/", "Prairies/", "British Columbia & Yukon/"};
-
-		// files for Canada region
-		outputCSV(filePath, dirHpstat[0] + "Canada/", getPeriod(hp_icu, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + "Canada/", getPeriod(hp_non_icu, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + "Canada/", getPeriod(non_hp, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + "Canada/", getPeriod(hp_icu, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + "Canada/", getPeriod(hp_non_icu, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + "Canada/", getPeriod(non_hp, colYear, colWeek, 21), colWeek);
-
-		// files for Atlantic region
-		outputCSV(filePath, dirHpstat[0] + dirRegion[0], getPeriod(AC1, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[0], getPeriod(AC2, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[0], getPeriod(AC3, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + dirRegion[0], getPeriod(AC1, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[0], getPeriod(AC2, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[0], getPeriod(AC3, colYear, colWeek, 21), colWeek);
-
-		// files for Quebec region
-		outputCSV(filePath, dirHpstat[0] + dirRegion[1], getPeriod(QC1, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[1], getPeriod(QC2, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[1], getPeriod(QC3, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + dirRegion[1], getPeriod(QC1, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[1], getPeriod(QC2, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[1], getPeriod(QC3, colYear, colWeek, 21), colWeek);
-
-		// files for Ontario & Nunavut region
-		outputCSV(filePath, dirHpstat[0] + dirRegion[2], getPeriod(ON1, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[2], getPeriod(ON2, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[2], getPeriod(ON3, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + dirRegion[2], getPeriod(ON1, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[2], getPeriod(ON2, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[2], getPeriod(ON3, colYear, colWeek, 21), colWeek);
-
-		// files for Prairies region
-		outputCSV(filePath, dirHpstat[0] + dirRegion[3], getPeriod(PS1, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[3], getPeriod(PS2, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[3], getPeriod(PS3, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + dirRegion[3], getPeriod(PS1, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[3], getPeriod(PS2, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[3], getPeriod(PS3, colYear, colWeek, 21), colWeek);
-
-		// files for British Columbia & Yukon region
-		outputCSV(filePath, dirHpstat[0] + dirRegion[4], getPeriod(BY1, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[4], getPeriod(BY2, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[4], getPeriod(BY3, colYear, colWeek, 20), colWeek);
-		outputCSV(filePath, dirHpstat[0] + dirRegion[4], getPeriod(BY1, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[1] + dirRegion[4], getPeriod(BY2, colYear, colWeek, 21), colWeek);
-		outputCSV(filePath, dirHpstat[2] + dirRegion[4], getPeriod(BY3, colYear, colWeek, 21), colWeek);
+		// generate and output files
+		outputCSV(hp_status_20);
+		outputCSV(hp_status_21);
 
 		/* Statistic outputs */
 		//todo
 	}
 
-	//|------------------------------------------------------------------------|
-	//|                                 METHODS                                |
-	//|------------------------------------------------------------------------|
+
+	//|------------------------------------------------------------------------|//
+	//|                                 METHODS                                |//
+	//|------------------------------------------------------------------------|//
+
+
+	/**
+	 * This method outputs .csv files by week, month, and quarter.
+	 * @param tables	the ArrayLists that need to be passed in
+	 */
+	private static void outputCSV(ArrayList<ArrayList<String[]>> tables)
+	{
+		String[] dir_hp_status = {"hp_status/hp_icu/", "hp_status/hp_non_icu/", "hp_status/non_hp/"};
+		String[] dir_region = {"CANADA/", "Atlantic/", "Quebec/",
+				"Ontario & Nunavut/", "Prairies/", "British Columbia & Yukon/"};
+		int colWeek = 2, colDeath = 15;
+
+		for (int i = 0; i < dir_region.length; i++) {
+			getCSV_week(tables, dir_hp_status, dir_region, colWeek, i);
+			getCSV_month(tables, dir_hp_status, dir_region, colWeek, i);
+			getCSV_quarter(tables, dir_hp_status, dir_region, colWeek, i);
+			getCSV_death(tables, dir_hp_status, dir_region, colDeath, i);
+		}
+	}
+
+	/**
+	 * This method generates .csv files by death values.
+	 * @param tables	the ArrayLists that need to be passed in
+	 * @param hpDir		directory of hospitalization status
+	 * @param regDir	directory of regions
+	 * @param dthCol	column number for COV_DTH
+	 * @param regVal	value of COV_REG
+	 */
+	private static void getCSV_death(ArrayList<ArrayList<String[]>> tables,
+									 String[] hpDir, String[] regDir, int dthCol, int regVal)
+	{
+		String[] dth_status_names = {"_death_", "_survival_"};
+
+		int regCol = 1, yrCol = 4;
+		for (int i = 0; i < tables.size(); i++) {
+			String fileDir = "outputs/" + hpDir[i] + regDir[regVal] + "death & survival/";
+			for (int j = 1; j <= dth_status_names.length; j++) {
+				String fileName = "20" + tables.get(i).get(j)[yrCol] + dth_status_names[j-1] + ".csv";
+				ArrayList<String[]> list = getEntry(tables.get(i), dthCol, j);
+				// select entries for each the 5 regions
+				if (regVal != 0) {
+					list = getEntry(list, regCol, regVal);
+				}
+				// avoid generating empty files
+				if (list.size() > 0) {
+					generateCSV(list, fileDir + fileName);
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method generates .csv files by week.
+	 * @param tables	the ArrayLists that need to be passed in
+	 * @param hpDir		directory of hospitalization status
+	 * @param regDir	directory of regions
+	 * @param wkCol		column number for COV_EW
+	 * @param regVal	value of COV_REG
+	 */
+	private static void getCSV_week(ArrayList<ArrayList<String[]>> tables,
+									String[] hpDir, String[] regDir, int wkCol, int regVal)
+	{
+		int regCol = 1, yrCol = 4;
+		for (int i = 0; i < tables.size(); i++) {
+			String fileDir = "outputs/" + hpDir[i] + regDir[regVal] + "weekly/";
+			for (int j = 0; j <= WEEKS; j++) {
+				String fileName = "20" + tables.get(i).get(j)[yrCol] + "_week_" + j + ".csv";
+				ArrayList<String[]> list = getEntry(tables.get(i), wkCol, j);
+				// select entries for each the 5 regions
+				if (regVal != 0) {
+					list = getEntry(list, regCol, regVal);
+				}
+				// avoid generating empty files
+				if (list.size() > 0) {
+					generateCSV(list, fileDir + fileName);
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method generates .csv files by month.
+	 * @param tables	the ArrayLists that need to be passed in
+	 * @param hpDir		directory of hospitalization status
+	 * @param regDir	directory of regions
+	 * @param wkCol		column number for COV_EW
+	 * @param regVal	value of COV_REG
+	 */
+	private static void getCSV_month(ArrayList<ArrayList<String[]>> tables,
+									 String[] hpDir, String[] regDir, int wkCol, int regVal)
+	{
+		// define each month of each year
+		int[][] month20 = { {0, 4}, {5, 8}, {9, 12}, {13, 17}, {18, 21}, {22, 25},
+				{26, 30}, {31, 34}, {35, 39}, {40, 43}, {44, 47}, {48, 52} };
+		int[][] month21 = { {0, 4}, {5, 8}, {9, 13}, {14, 17}, {18, 21}, {22, 26},
+				{27, 30}, {31, 34}, {35, 39}, {40, 43}, {44, 47}, {48, 52} };
+		int[][] month = month20;	// for swap purposes
+		int regCol = 1, yrCol = 4;
+
+		for (int i = 0; i < tables.size(); i++) {
+			String fileDir = "outputs/" + hpDir[i] + regDir[regVal] + "monthly/";
+
+			// choose the correct month definition
+			if (tables.get(i).get(0)[yrCol].equals("21")) {
+				month = month21;
+			} else if (tables.get(i).get(0)[yrCol].equals("20")) {
+				month = month20;
+			}
+
+			for (int j = 0; j < month.length; j++) {
+				String fileName = "20" + tables.get(i).get(j)[yrCol] + "_month_" + (j + 1)
+						+ " (week_" + month[j][0] + "-" + month[j][1] + ").csv";
+				ArrayList<String[]> list = new ArrayList<>();
+				// merge entries in a same quarter
+				for (int k = month[j][0]; k <= month[j][1]; k++) {
+					list.addAll(getEntry(tables.get(i), wkCol, k));
+				}
+				// select entries for each the 5 regions
+				if (regVal != 0) {
+					list = getEntry(list, regCol, regVal);
+				}
+				// avoid generating empty files
+				if (list.size() > 0) {
+					generateCSV(list, fileDir + fileName);
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method generates .csv files by quarter.
+	 * @param tables	the ArrayLists that need to be passed in
+	 * @param hpDir		directory of hospitalization status
+	 * @param regDir	directory of regions
+	 * @param wkCol		column number for COV_EW
+	 * @param regVal	value of COV_REG
+	 */
+	private static void getCSV_quarter(ArrayList<ArrayList<String[]>> tables,
+									   String[] hpDir, String[] regDir, int wkCol, int regVal)
+	{
+		// define each quarter of a year
+		int[][] quarter = { {1, 13}, {14, 26}, {27, 39}, {40, 52} };
+		int regCol = 1, yrCol = 4;
+
+		for (int i = 0; i < tables.size(); i++) {
+			String fileDir = "outputs/" + hpDir[i] + regDir[regVal] + "quarterly/";
+			for (int j = 0; j < quarter.length; j++) {
+				String fileName = "20" + tables.get(i).get(j)[yrCol] + "_quarter_" + (j + 1)
+						+ " (week_" + quarter[j][0] + "-" + quarter[j][1] + ").csv";
+				ArrayList<String[]> list = new ArrayList<>();
+				// merge entries in a same quarter
+				for (int k = quarter[j][0]; k <= quarter[j][1]; k++) {
+					list.addAll(getEntry(tables.get(i), wkCol, k));
+				}
+				// select entries for each the 5 regions
+				if (regVal != 0) {
+					list = getEntry(list, regCol, regVal);
+				}
+				// avoid generating empty files
+				if (list.size() > 0) {
+					generateCSV(list, fileDir + fileName);
+				}
+			}
+		}
+	}
 
 	/**
 	 * This methods splits the original data by values of years then output them.
-	 * @param path	path to output new .csv files
 	 * @param table	the ArrayList that needs to be passed in
 	 * @param col	the column number for search, depends on the chosen key
 	 * @param key	the key value for search (i.e. which year)
 	 */
-	private static void splitCSV(String path, ArrayList<String[]> table, int col, int key)
+	private static void splitCSV_year(ArrayList<String[]> table, int col, int key)
 	{
 		String name = "outputs/year_" + key + ".csv";
 		ArrayList<String[]> temp = getEntry(table, col, key);
 		if (temp.size() > 0) {
-			generateCSV(path + name, temp);
+			generateCSV(temp, name);
 		}
-	}
-
-
-	private static void outputCSV2(String path, String ctg, ArrayList<String[]> table, int wkCol)
-	{
-		// generate files by each month
-
-
-	}
-
-
-	/**
-	 * This method generates .csv files contained in different directories based on needs.
-	 * @param path	path to output new .csv files
-	 * @param ctg	name of subdirectory (i.e. hospitalization status)
-	 * @param table	the ArrayList that needs to be passed in
-	 * @param wkCol	column number for COV_EW
-	 */
-	private static void outputCSV(String path, String ctg, ArrayList<String[]> table, int wkCol)
-	{
-		// generate files by each week
-		for (int i = startWeek; i <= WEEKS; i++) {
-			String name = "outputs/" + ctg + "perWeek/" + nameYear + "week_" + i + ".csv";
-			ArrayList<String[]> temp = getEntry(table, wkCol, i);
-			// avoid generating empty files
-			if (temp.size() > 0) {
-				generateCSV(path + name, temp);
-			}
-		}
-
-		//todo
-
-		// generate files by each month
-		for (int i = startWeek; i <= WEEKS; i += 5) {
-			String name = "outputs/" + ctg + "per5Week/" + nameYear + "weeks_" + i + "-" + (i + 4) + ".csv";
-			ArrayList<String[]> temp = getEntry(table, wkCol, i);
-			for (int j = i + 1; j <= i + 4; j++) {
-				temp.addAll(getEntry(table, wkCol, j));
-			}
-			// avoid generating empty files
-			if (temp.size() > 0) {
-				generateCSV(path + name, temp);
-			}
-		}
-
-		// generate files by each quarter
-		for (int i = startWeek; i <= WEEKS; i += 13) {
-			String name = "outputs/" + ctg + "perQuarter/" + nameYear + "quarter_" + (i + 12)/13 + ".csv";
-			ArrayList<String[]> temp = getEntry(table, wkCol, i);
-			for (int j = i + 1; j <= i + 12; j++) {
-				temp.addAll(getEntry(table, wkCol, j));
-			}
-			// avoid generating empty files
-			if (temp.size() > 0) {
-				generateCSV(path + name, temp);
-			}
-		}
-	}
-
-	/**
-	 * This methods splits entries by time period then store them into ArrayLists.
-	 * @param table	the ArrayList that needs to be passed in
-	 * @param yrCol	the column number recording episode year values
-	 * @param wkCol	the column number recording episode week values
-	 * @param year	episode year values
-	 * @return	an ArrayList
-	 */
-	private static ArrayList<String[]> getPeriod(ArrayList<String[]> table, int yrCol, int wkCol, int year)
-	{
-		// name the years and choose the start week for each file
-		if (year == 20) {
-			nameYear = "2020_";
-			startWeek = 8;
-		} else if (year == 21) {
-			nameYear = "2021_";
-			startWeek = 1;
-		}
-
-		// traverse the table passed in, find the valid entries
-		ArrayList<String[]> list = new ArrayList<>();
-		for (String[] entry : getEntry(table, yrCol, year)) {
-			if (Integer.parseInt(entry[wkCol]) >= 1 && Integer.parseInt(entry[wkCol]) <= WEEKS) {
-				list.add(entry);
-			}
-		}
-		return list;
 	}
 
 	/**
@@ -250,7 +273,7 @@ public class csvProcessor
 	 * @param path	path to output new .csv files
 	 * @param table the ArrayList that needs to be passed in
 	 */
-	private static void generateCSV(String path, ArrayList<String[]> table)
+	private static void generateCSV(ArrayList<String[]> table, String path)
 	{
 		String token1 = "[", token2 = "]", token3 = "";
 		BufferedWriter bw = null;
